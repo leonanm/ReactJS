e202b52b4dba9556333146eaebb2da9da7d4cd83
Leonan Moreira Martins <leonanmoreira@hotmail.com>
2023-01-13 22:14:26 -0300

    React Router ROM

---
+++ 02-ignite-timer-master/src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './components/Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
