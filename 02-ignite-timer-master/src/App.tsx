e5d474d2b91125670109e5e3bad7fc3e597d7412
leonanm <leonanmoreira@hotmail.com>
2023-01-16 11:37:16 -0300

    Configurando o Layout de rotas | Header & Layout

--- 02-ignite-timer-master/src/App.tsx
+++ 02-ignite-timer-master/src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-import { Router } from './components/Router'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
