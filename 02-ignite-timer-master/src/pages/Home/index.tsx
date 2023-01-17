eb69d47f1b5565a61e6b12a9a1ecb99f4cb1c308
leonanm <leonanmoreira@hotmail.com>
2023-01-17 15:55:26 -0300

    Aprimoramento dos inputs | History | Status

--- 02-ignite-timer-master/src/pages/Home/index.tsx
+++ 02-ignite-timer-master/src/pages/Home/index.tsx
@@ -15,13 +15,28 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <TaskInput type="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput
+            type="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>Minutos</span>
